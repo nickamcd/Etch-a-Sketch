07d6dfa1331e8310cc04c16c69f73300a8f5563f
nickamcd <nickamcd@gmail.com>
2020-01-10 14:20:00 -0800

    Added grid of squares to page.

--- myScript.js
+++ myScript.js
@@ -1,4 +1,19 @@
+const GRID_SIZE = 16;
+
 function populateGrid() {
     const container = document.querySelector("#container");
-    
+    let docFragment = document.createDocumentFragment();
+
+    for (i = 0; i < GRID_SIZE; i++) {
+        for (j = 0; j < GRID_SIZE; j++) {
+            const div = document.createElement("div");
+            div.classList.add("square");
+            docFragment.appendChild(div);
+        }
+    }
+
+    container.appendChild(docFragment);
 }
+
+
+populateGrid();
