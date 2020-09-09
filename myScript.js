731f1989251418d37ef05973383fb4d939015285
Nick McDaniel <nickamcd@gmail.com>
2020-09-08 18:21:23 -0700

    Commit of progress to pull from VM

--- myScript.js
+++ myScript.js
@@ -1,11 +1,11 @@
-const GRID_SIZE = 16;
+let gridSize = 16;
 
 function populateGrid() {
     const container = document.querySelector("#container");
     let docFragment = document.createDocumentFragment();
 
-    for (i = 0; i < GRID_SIZE; i++) {
-        for (j = 0; j < GRID_SIZE; j++) {
+    for (i = 0; i < gridSize; i++) {
+        for (j = 0; j < gridSize; j++) {
             const div = document.createElement("div");
             div.classList.add("square");
             docFragment.appendChild(div);
